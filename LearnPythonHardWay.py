a47c9cde83dc1e890595da1fc5d2cb1a12d16ac6
Travis <maxtiff@users.noreply.github.com>
2013-10-21 15:36:48 -0500

    Update LearnPythonHardWay.py

--- LearnPythonHardWay.py
+++ LearnPythonHardWay.py
@@ -1,18 +1,33 @@
-def print_two(*args):
-	arg1, arg2 = args
-	print ("arg1: {0}, arg2: {1}".format(arg1, arg2))
+from sys import argv
 
+script, input_file = argv
 
-def print_two_better(arg1, arg2):
-	print ("arg1: {0}, arg2: {1}".format(arg1, arg2))
+def print_all(f):
+	print (f.read())
 
-def print_one(arg1):
-	print ("arg1: {0}".format(arg1))
+def rewind(f):
+	f.seek(0)
 
-def print_none():
-	print ("I got nothin'.")
+def print_a_line(line_count, f):
+	print (line_count, f.readline())
 
-print_two('Travis', 'May')
-print_two_better('Travis', 'May')
-print_one('First!')
-print_none()
+current_file = open(input_file)
+
+print ("First let's print the whole file: \n")
+
+print_all(current_file)
+
+print ("Now let's rewind, kindof like a tape.")
+
+rewind(current_file)
+
+print ("Let's print three lines.")
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
